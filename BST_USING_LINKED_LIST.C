1944a0f9d138c3a4ba0a2cad5756cbf209f513f1
Rishabh Singh <113104888+rishabhcse045@users.noreply.github.com>
2023-10-19 14:36:43 +0530

    Create BST_USING_LINKED_LIST.C

---
+++ BST_USING_LINKED_LIST.C
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+struct TreeNode* search(struct TreeNode* root, int key) {
+    if (root == NULL || root->data == key) {
+        return root;
+    }
+
+    if (key < root->data) {
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
+void inorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+    int n, data;
+
+    printf("Enter the number of elements to insert: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+
+    printf("Inorder traversal of the BST: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    int key;
+    printf("Enter the key to search for: ");
+    scanf("%d", &key);
+
+    if (search(root, key)) {
+        printf("%d is found in the BST.\n", key);
+    } else {
+        printf("%d is not found in the BST.\n", key);
+    }
+
+    return 0;
+}
